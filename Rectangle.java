e13483129a32b7e228802d8f9d12b1c0b1fe7307
neelswarnkar <81668866+neelswarnkar@users.noreply.github.com>
2021-03-30 21:57:01 -0700

    Add files via upload

---
+++ Rectangle.java
@@ -0,0 +1,28 @@
+
+public class Rectangle {
+	int length;
+	int width;
+	
+	void insert(int l, int w) {
+		length=l;
+		width=w;
+	}
+	void calculateArea() {
+		System.out.println(length*width);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Rectangle r1=new Rectangle();
+		Rectangle r2=new Rectangle();
+		
+		r1.insert(12, 23);
+		r2.insert(54, 5);
+		
+		r1.calculateArea();
+		r2.calculateArea();
+
+	}
+
+}
